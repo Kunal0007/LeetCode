2585d1c4a913ae9251a17e670d7bf0888f8c4c90
Kunal Patil <kunal.kv1dr64@gmail.com>
2021-09-16 11:21:04 +0530

    Problem2:Two Sum

---
+++ Two_Sum.java
@@ -0,0 +1,21 @@
+package com.company.LeetCode;
+
+import java.util.Arrays;
+
+public class Two_Sum {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,3};
+        int[] index = new int[2];
+        int target = 9;
+        for (int i = 0; i<arr.length; i++){
+            for (int j = i + 1; j<arr.length; j++){
+                if (arr[i] + arr[j] == 6){
+                    index[0] = i;
+                    index[1] = j;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(index));
+    }
+}
