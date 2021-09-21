e339f443ad643bceefa2a011c06346c824eefce7
Kunal Patil <kunal.kv1dr64@gmail.com>
2021-09-21 23:17:36 +0530

    Problem5: Richest Customer Wealth

---
+++ Richest_Customer_Wealth.java
@@ -0,0 +1,26 @@
+package com.company.LeetCode;
+
+public class Richest_Customer_Wealth {
+    public static void main(String[] args) {
+        int[][] accounts = {
+                {1, 5},
+                {7, 3},
+                {3, 5}
+        };
+        System.out.println(maxwealth(accounts));
+    }
+
+    private static int maxwealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int[] account : accounts) {
+            int wealth = 0;
+            for (int i : account) {
+                wealth = wealth + i;
+            }
+            if (wealth > max) {
+                max = wealth;
+            }
+        }
+        return max;
+    }
+}
