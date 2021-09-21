31be8bd2f0e6be1269d0775e9fbe1279b15e154d
Kunal Patil <kunal.kv1dr64@gmail.com>
2021-09-21 22:45:04 +0530

    Problem4: Find Numbers with Even Number of Digits

--- EvenNumber_ofDigits.java
+++ EvenNumber_ofDigits.java
@@ -6,6 +6,8 @@ public class EvenNumber_ofDigits {
     public static void main(String[] args) {
         int[] arr = {12,345,25,6,7896};
         System.out.println(evendigitno(arr));
+
+//        System.out.println(digits(432));
     }
 
     private static int evendigitno(int[] arr) {
@@ -24,11 +26,13 @@ public class EvenNumber_ofDigits {
     }
 
     private static int digits(int num) {
-        int count = 0;
-        while (num > 0){
-            count++;
-            num = num / 10;
-        }
-        return count;
+//        int count = 0;
+//        while (num > 0){
+//            count++;
+//            num = num / 10;
+//        }
+//        return count;
+
+        return (int)(Math.log10(num)) + 1;
     }
 }
