bc2bb33082367a964b819a085526bde41f52c9ad
Kunal Patil <kunal.kv1dr64@gmail.com>
2021-09-27 12:17:20 +0530

    Problem6: Find Smallest Letter Greater Than Target

---
+++ Smallest_Letter_Greater.java
@@ -0,0 +1,29 @@
+package com.company.LeetCode;
+
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+public class Smallest_Letter_Greater {
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j', 'k', 'n', 'r'};
+        char target = 'f';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+
+    static char nextGreatestLetter(char[] letters, char target){
+        int start = 0;
+        int end = letters.length - 1;
+
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]){
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
