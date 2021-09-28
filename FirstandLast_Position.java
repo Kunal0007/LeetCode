57a1f969361281bc52ecde0e1eb1730edf51f5ab
Kunal Patil <kunal.kv1dr64@gmail.com>
2021-09-28 11:30:04 +0530

    Problem7: Find First and Last Position of Element in Sorted Array

--- FirstandLast_Position.java
+++ FirstandLast_Position.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class FirstandLast_Position {
     public static void main(String[] args) {
-        int[] nums = {5,7,7,7,7,8,8,8,10};
-        int target = 1;
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
 
