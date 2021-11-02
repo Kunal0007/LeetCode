a524543b62761e8e40117a2b809c04622dd4b4bf
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-11-02 11:52:47 +0530

    Problem24: Kids With the Greatest Number of Candies

--- GreatestNo_Candies.java
+++ GreatestNo_Candies.java
@@ -14,7 +14,7 @@ public class GreatestNo_Candies {
     }
 
     static public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> ans = new ArrayList<Boolean>(candies.length);
+        List<Boolean> ans = new ArrayList<Boolean>();
         int greatest = 0;
         for (int i = 0; i < candies.length; i++) {
             if (candies[i] > candies[greatest]){
