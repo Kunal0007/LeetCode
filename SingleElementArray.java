fcfc889eef874450f2b45ab80272d68bb532d5ae
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-11-30 22:52:14 +0530

    Problem42: Single Element in a Sorted Array

---
+++ SingleElementArray.java
@@ -0,0 +1,18 @@
+package com.company.LeetCode;
+
+// https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+public class SingleElementArray {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(nums));
+    }
+
+    static public int singleNonDuplicate(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
