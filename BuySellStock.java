262195431c967692d2cc2511e8e34b71957fb03d
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-08-01 00:07:14 +0530

    Problem52: Best Time to Buy and Sell Stock

---
+++ BuySellStock.java
@@ -0,0 +1,24 @@
+package com.company.LeetCode;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+public class BuySellStock {
+    public static void main(String[] args) {
+        int[] prices = {4,5,6,3,1,4};
+        System.out.println(maxProfit(prices));
+    }
+
+    static public int maxProfit(int[] prices) {
+        int profit = 0;
+        int fix = prices[prices.length - 1];
+        for (int i = prices.length - 2; i >= 0; i--) {
+            if (fix - prices[i] >= profit){
+                profit = fix - prices[i];
+            }
+            else if (fix - prices[i] < 0){
+                fix  = prices[i];
+            }
+        }
+        return profit;
+    }
+}
